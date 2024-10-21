4a47aa78b494acdaf5bd47a4ab9db60652a85a51
GreenHugeBrain <142096498+GreenHugeBrain@users.noreply.github.com>
2024-10-21 14:05:13 +0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,12 +2,13 @@
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-
+    CORS(app)
     # App configuration
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db' 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
