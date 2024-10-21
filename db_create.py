5bdd2b53376ef6d717faf906e06887455da24fef
GreenHugeBrain <142096498+GreenHugeBrain@users.noreply.github.com>
2024-10-21 14:01:21 +0400

    Add files via upload

---
+++ db_create.py
@@ -0,0 +1,9 @@
+from __init__ import create_app, db
+from models import Student, Subject, Grade
+
+app = create_app()
+
+def create_db():
+    with app.app_context():
+        db.create_all() 
+        print("Database created successfully!")
